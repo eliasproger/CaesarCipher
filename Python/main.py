0b7a40b5d45ddb972a55bcab4ab90e441dcaeb42
illaportable <90960659+illaportable@users.noreply.github.com>
2022-09-14 22:11:27 +0300

    Change file

--- Python/main.py
+++ Python/main.py
@@ -1,23 +1,36 @@
-ua_letters = ['а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р',
-              'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я']
+import string
 
 
-def coder(text: str, offset=1):
+
+def coder(txt, offset=1):
     coded_text = ''
-
-    for i, v in enumerate(text.lower().strip()):
-        coded_text += v if v == ' ' or v in '1234567890' else str(ua_letters[0 if v == 'я' else ua_letters.index(v) + offset])
+    for i in txt:
+        if i in 'я' and i.islower():
+            coded_text = chr(ord(i)-31)
+        elif i in 'Я' and i.isupper():
+            coded_text = chr(ord(i)-63+32)
+        elif i in string.punctuation+string.digits+' ':
+            coded_text += i
+        else:
+            coded_text += chr(ord(i) + offset) if i != ' ' else i
     return coded_text
 
-
-def decoder(text: str, offset=1):
+def decoder(txt, offset=1):
     decoded_text = ''
-    for i, v in enumerate(text.lower().strip()):
-        decoded_text += v if v == ' ' or v in '1234567890' else str(ua_letters[0 if v == 'я' else ua_letters.index(v) - offset])
+    for i in txt:
+        if i == 'а' and i.islower():
+            decoded_text += chr(ord(i)+31)
+        elif i == 'А' and i.isupper():
+            decoded_text += chr(ord(i)+31)
+        elif i in string.punctuation+string.digits+' ':
+            decoded_text += i
+        else:
+            decoded_text += chr(ord(i) - offset) if i != ' ' else i
     return decoded_text
 
-
 if __name__ == '__main__':
-    text = input("Enter text: ")
+    print("Ти можеш Вибрати: \n\"К\" якщо Кодувати чи \"Д\" якщо Декодувати")
+    whats = input("Що ти хочеш робити: ")
+    text = input("Введи текст: ")
     # print decoder or coder
-    print(decoder('а леп'))
+    print(coder(text))
